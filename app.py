694c849aea8fb946fce781ab2ff1f187eccdabb1
Bdgdirect1k <125243759+Bdgdirect1k@users.noreply.github.com>
2023-05-05 09:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://Bdgdirect1k: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@github.com/Bdgdirect1k/Mogenius okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Bdgdirect1k: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@github.com/Bdgdirect1k/Mogenius okk && cd okk && pip3 install -U -r requirements.txt && nohup Bot().run() &")
